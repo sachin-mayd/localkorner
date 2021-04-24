47fc7aabf69e421575eaba50d81ca6a5672e1f5d
shailirock123 <pratap.shailendra9@gmail.com>
2021-04-24 20:22:15 +0530

    validation done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -44,7 +44,8 @@ import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
    
     
   ],
